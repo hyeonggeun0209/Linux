2ce21f3e263e1429445b76875301a43b883e1c18
hyeonggeun0209 <55240146+hyeonggeun0209@users.noreply.github.com>
2019-11-05 11:30:31 +0900

    Add files via upload

---
+++ ex/build/src/CMakeFiles/my_prog.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my_prog.dir/main.cpp.o"
+  "my_prog.pdb"
+  "my_prog"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/my_prog.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ex/lib/util.cpp
@@ -0,0 +1,4 @@
+#include "util.h"
+void print_message(void) {
+    printf("Hello World!\n");
+}
