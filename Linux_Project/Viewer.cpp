847e1e3ae6c0bced28c2354a0b0072171b363cfd
hyeonggeun0209 <55240146+hyeonggeun0209@users.noreply.github.com>
2019-11-25 21:03:00 +0900

    Update Viewer.cpp

--- Linux_Project/Viewer.cpp
+++ Linux_Project/Viewer.cpp
@@ -1,5 +1,5 @@
 #include "Viewer.h"
 
-Viewer::Viewer(string PhoneNumber) { }
+void Viewer::set(string PhoneNumber) { }
 void Viewer::vote(string name1, string name2) { }
 void Viewer::donate(int money) { }
